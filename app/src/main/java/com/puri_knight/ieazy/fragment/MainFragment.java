32ef0953170c18b51ea858d46250caeacffa5cce
PuriKnight <Puri.knight379@gmail.com>
2017-10-08 10:21:07 +0700

    Start 8 Oct 2017

--- app/src/main/java/com/puri_knight/ieazy/fragment/MainFragment.java
+++ app/src/main/java/com/puri_knight/ieazy/fragment/MainFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment; // should get fragment by Thian
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.puri_knight.ieazy.R;
 
@@ -16,11 +18,62 @@ import java.util.zip.Inflater;
  */
 
 public class MainFragment extends Fragment{
+    //Explicit
+    private ImageView imageView;
+    private Button decButton, incButton;
+// My Constant
+
+
+    private int[] imageInts = new int[]{
+            R.drawable.iconluv1,
+            R.drawable.iconluv2,
+            R.drawable.iconluv3,
+    };
+
+    private int indexInt = 0;
+
+
     @Nullable
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main,container,false);
         //container get all container
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+         // Initial Veiw
+                 initialView();
+
+        // Increase Controller
+        increaseController();
+
+
+    }
+
+    private void increaseController() {
+        incButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+
+
+
+
+            } // OnClick
+        });
+    }
+
+    private void initialView() {
+        imageView = (ImageView) getView().findViewById(R.id.imvImage);
+        decButton = (Button) getView().findViewById(R.id.btnDecrease);
+        incButton = (Button) getView().findViewById(R.id.btnIncrease);
     }
 }// Main Class
